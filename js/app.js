8c1b29c69541dc9bfb63f08165eecb9abcd65ed8
Travis Wilson <psyon001@gmail.com>
2013-07-11 07:18:34 -0400

    Changed design pattern and more testing

--- js/app.js
+++ js/app.js
@@ -1,16 +1,39 @@
 'use strict';
 
+var devSlides = angular.module('devSlides.app', []);
 
-// Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
-	config(['$routeProvider', function($routeProvider) {
-		$routeProvider.when('/view1', {
-			templateUrl: 'partials/partial1.html',
-			controller: 'slidePreview'
-		});
-		$routeProvider.when('/view2', {
-			templateUrl: 'partials/partial2.html',
-			controller: 'MyCtrl2'
-		});
-		$routeProvider.otherwise({redirectTo: '/view1'});
+devSlides.log = angular.module('devSlides.log', []);
+
+angular.module('devSlides', ['devSlides.app']).config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/home', {
+		templateUrl: 'partials/partial1.html',
+		controller: 'slidePreview'
+	});
+	// $routeProvider.when('/home', {
+	// 	templateUrl: 'partials/partial2.html',
+	// 	controller: 'editorFrame'
+	// });
+	$routeProvider.otherwise({redirectTo: '/home'});
 }]);
+
+
+
+
+
+// // Declare app level module which depends on filters, and services
+// angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+// 	config(['$routeProvider', function($routeProvider) {
+// 		$routeProvider.when('/home', {
+// 			templateUrl: 'partials/partial1.html',
+// 			controller: 'slidePreview'
+// 		});
+// 		$routeProvider.when('/home', {
+// 			templateUrl: 'partials/partial2.html',
+// 			controller: 'editorFrame'
+// 		});
+// 		$routeProvider.when('/view2', {
+// 			templateUrl: 'partials/partial2.html',
+// 			controller: 'MyCtrl2'
+// 		});
+// 		$routeProvider.otherwise({redirectTo: '/home'});
+// }]).run(function($route){});
--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,19 @@
 'use strict';
 
 /* Controllers */
+devSlides.controller('slidePreview',['$scope', '$http', function($scope, $http){
+	$scope.name = 'Travis Wilson';
+}]);
+devSlides.controller('editorFrame',['$scope', '$http', function($scope, $http){
+	$scope.email = 'traviswilson@gmail.com';
+}]);
 
-angular.module('myApp.controllers', []).
-	controller('slidePreview', [function() {
-	}])
-	.controller('MyCtrl2', [function() {
-	}]);
+// angular.module('myApp.controllers', []).
+// 	controller('slidePreview', function($scope) {
+// 		$scope.name = 'Travis Wilson';
+// 	})
+// 	.controller('editorFrame', function($scope) {
+// 		$scope.email = 'traviswilson@gmail.com';
+// 	})
+// 	.controller('MyCtrl2', [function() {
+// 	}]);
